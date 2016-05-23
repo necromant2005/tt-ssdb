08ef4cba1d4a9102ab2c633d6e8957d6aba2a719
Rostislav Mykhajliw <necromant2005@gmail.com>
2016-05-23 09:06:28 +0300

    ssdb

--- src/TweeSSDB/Cache/Storage/Adapter/SSDB.php
+++ src/TweeSSDB/Cache/Storage/Adapter/SSDB.php
@@ -90,8 +90,13 @@ class SSDB extends AbstractAdapter implements
         $servers = $options->getSlaveServers();
         shuffle($servers);
         $server = reset($servers);
-        $SSDB->connect($server['host'], $server['port']);
-        $SSDB->option(SsdbResource::OPT_SERIALIZER, SsdbResource::SERIALIZER_PHP);
+
+        try {
+            $SSDB->connect($server['host'], $server['port']);
+            $SSDB->option(SsdbResource::OPT_SERIALIZER, SsdbResource::SERIALIZER_PHP);
+        } catch (\Exception $e) {
+            throw new \RuntimeException('Server ' . $server['host'] . ':' . $server['port'] . ' out of order', 100, $e);
+        }
 
         // use the initialized resource
         $this->SSDBSlaveResource = $SSDB;
